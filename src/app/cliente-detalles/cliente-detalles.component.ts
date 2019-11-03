dbd23d0257392bb0b5f39cd7aae58926c7dd78d3
Marlon Aguirre <marlon.aguirre.dev@gmail.com>
2019-11-03 01:43:51 -0600

    crear y editar cliente

--- src/app/cliente-detalles/cliente-detalles.component.ts
+++ src/app/cliente-detalles/cliente-detalles.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ClientesService } from '../servicios/clientes.service';
+import { ActivatedRoute } from '@angular/router';
+import { MatSnackBar } from '@angular/material';
 
 @Component({
   selector: 'app-cliente-detalles',
@@ -7,9 +10,67 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClienteDetallesComponent implements OnInit {
 
-  constructor() { }
+  cliente: any;
+
+  constructor(
+    private route: ActivatedRoute,
+    private clienteService: ClientesService,
+    private snackBar: MatSnackBar
+  ) { }
 
   ngOnInit() {
+    this.route.paramMap.subscribe(params => {
+      this.obtenerCliente(params.get('clienteId'));
+    });
+  }
+  obtenerCliente(id: string) {
+    return this.clienteService.obtenerCliente(id).subscribe(resultado => {
+      this.cliente = resultado;
+    });
+  }
+
+  toggleModalEditar() {
+    let button = document.getElementById("toggleModalEditarCliente");
+    button.click();
+    (<HTMLInputElement>document.getElementById("clienteNombreE")).value = this.cliente.nombre;
+    (<HTMLInputElement>document.getElementById("clienteApellidoE")).value = this.cliente.apellido;
+    (<HTMLInputElement>document.getElementById("clienteCorreoE")).value = this.cliente.correo;
+    (<HTMLInputElement>document.getElementById("clienteDireccionE")).value = this.cliente.direccion;
+    (<HTMLInputElement>document.getElementById("clienteEstadoE")).value = this.cliente.estado;
+    (<HTMLInputElement>document.getElementById("clienteOrigenE")).value = this.cliente.origen;
+  }
+
+  editarCliente(): void {
+    //let nombre = document.getElementById("rolNombre");
+    let nombre = (<HTMLInputElement>document.getElementById("clienteNombreE")).value;
+    let apellido = (<HTMLInputElement>document.getElementById("clienteApellidoE")).value;
+    let correo = (<HTMLInputElement>document.getElementById("clienteCorreoE")).value;
+    let direccion = (<HTMLInputElement>document.getElementById("clienteDireccionE")).value;
+    let estado = (<HTMLInputElement>document.getElementById("clienteEstadoE")).value;
+    let origen = (<HTMLInputElement>document.getElementById("clienteOrigenE")).value;
+
+    if (nombre == "" || apellido == "" || direccion == "" ) {
+      this.snackBar.open('Campos obligatorios! [nombre,apellido,direccion]', 'X', {
+        duration: 3000
+      });
+    } else {
+      this.cliente.nombre = nombre;
+      this.cliente.estado = estado;
+      this.cliente.apellido = apellido;
+      this.cliente.correo = correo;
+      this.cliente.direccion = direccion;
+      this.cliente.origen = origen;
+      console.log("cliente "+ JSON.stringify(this.cliente) )
+
+      this.clienteService.editarCliente(this.cliente).subscribe((resultado) => {
+        this.toggleModalEditar();
+        this.snackBar.open('cliente actualizado con exito!', 'X', {
+          duration: 3000
+        });
+      }, (error) => {
+        console.log("Error: " + JSON.stringify(error));
+      });
+    }
   }
 
 }
